324dfd047b9f6db161424bbcb48a1f25d763af80
Andrey_Zhidkov <a.zhidkov@sap.com>
2026-03-21 21:11:31 +0100

    renamed rg location

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "rg" {
 module "vnet" {
   source    = "git::https://github.com/AndrewDvizhok/terraform-learning.git?ref=vnet"
   vnet_name = "my-tf-vnet"
-  location  = module.rg.location
+  location  = module.rg.rg_location
   rgn       = module.rg.rg_name
 
 }
