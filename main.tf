e572150a875506f94f8fc69176327a72d81987d6
Andrey_Zhidkov <a.zhidkov@sap.com>
2026-03-21 14:59:50 +0100

    test main with module from github

--- main.tf
+++ main.tf
@@ -11,3 +11,14 @@ provider "azurerm" {
   features {}
 }
 
+module "rg"{
+	source = "./modules/rg"
+}
+
+module "vnet" {
+source = "git::https://github.com/AndrewDvizhok/terraform-learning.git?ref=vnet"
+vnet_name = "my-tf-vnet"
+location = azurerm_resource_group.default.location
+rgn = azurerm_resource_group.default.name
+
+}
