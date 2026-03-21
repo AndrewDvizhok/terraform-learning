7911447bfaf29341f57a695a655af0accc9f423e
Andrey_Zhidkov <a.zhidkov@sap.com>
2026-03-21 19:56:55 +0100

    Use vars from git module

--- main.tf
+++ main.tf
@@ -11,14 +11,14 @@ provider "azurerm" {
   features {}
 }
 
-module "rg"{
-	source = "./modules/rg"
+module "rg" {
+  source = "./modules/rg"
 }
 
 module "vnet" {
-source = "git::https://github.com/AndrewDvizhok/terraform-learning.git?ref=vnet"
-vnet_name = "my-tf-vnet"
-location = azurerm_resource_group.default.location
-rgn = azurerm_resource_group.default.name
+  source    = "git::https://github.com/AndrewDvizhok/terraform-learning.git?ref=vnet"
+  vnet_name = "my-tf-vnet"
+  location  = module.rg.location
+  rgn       = module.rg.rg_name
 
 }
