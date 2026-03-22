6b836b49fa214c1d99b46f745dbd9578991ae66d
Andrey_Zhidkov <a.zhidkov@sap.com>
2026-03-22 15:45:29 +0100

    put tags

--- main.tf
+++ main.tf
@@ -16,11 +16,13 @@ module "rg" {
 }
 
 module "vnet" {
-for_each = toset(["first", "second", "third"])
+  for_each   = toset(["first", "second", "third"])
   source     = "git::https://github.com/AndrewDvizhok/terraform-learning.git?ref=vnet"
   vnet_name  = "my-tf-vnet-${each.key}"
   location   = module.rg.rg_location
   rgn        = module.rg.rg_name
   depends_on = [module.rg]
-  
+  tags {
+    env = "test"
+  }
 }
