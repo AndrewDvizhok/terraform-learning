e2270f6cead81f2fb8766d047075a6fa3eba09d5
Andrey_Zhidkov <a.zhidkov@sap.com>
2026-03-22 14:00:57 +0100

    added for_each method for vnet module

--- main.tf
+++ main.tf
@@ -16,9 +16,11 @@ module "rg" {
 }
 
 module "vnet" {
+for_each = toset(["first", "second", "third"])
   source     = "git::https://github.com/AndrewDvizhok/terraform-learning.git?ref=vnet"
-  vnet_name  = "my-tf-vnet"
+  vnet_name  = "my-tf-vnet-${each.key}"
   location   = module.rg.rg_location
   rgn        = module.rg.rg_name
   depends_on = [module.rg]
+  
 }
