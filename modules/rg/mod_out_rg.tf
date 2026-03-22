5a9de31ffd93e715fb7e5642345b686b592795ca
Andrey_Zhidkov <a.zhidkov@sap.com>
2026-03-22 13:54:11 +0100

    added module rg support files

---
+++ modules/rg/mod_out_rg.tf
@@ -0,0 +1,11 @@
+output "rg_id"{
+	value = azurerm_resource_group.default.id
+}
+
+output "rg_name"{
+	value = azurerm_resource_group.default.name
+}
+
+output "rg_location"{
+	value = azurerm_resource_group.default.location
+}
