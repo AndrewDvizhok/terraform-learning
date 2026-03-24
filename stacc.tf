1162753e6ea02aab8c5262270cb81883bbfb67fd
Andrey_Zhidkov <a.zhidkov@sap.com>
2026-03-24 14:27:52 +0100

    storage account module

---
+++ stacc.tf
@@ -0,0 +1,18 @@
+variable "rgn" {}
+variable "name" {
+  default = "andrewdvizhoktflearnsc"
+}
+resource "azurerm_storage_account" "tf" {
+  name                     = var.name
+  resource_group_name      = var.rgn
+  location                 = "spaincentral"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+}
+
+resource "azurerm_storage_container" "tf" {
+  name                  = "state"
+  storage_account_name  = azurerm_storage_account.tf.name
+  container_access_type = "private"
+}
